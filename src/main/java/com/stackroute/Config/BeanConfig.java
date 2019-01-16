520c68d45483f93f4d290fdff3cb96e9f94eb5b1
Akshay <akshayvb66@gmail.com>
2019-01-16 15:28:53 +0530

    task2

--- src/main/java/com/stackroute/Config/BeanConfig.java
+++ src/main/java/com/stackroute/Config/BeanConfig.java
@@ -4,41 +4,47 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanConfig {
 
     @Bean("actor")
-    public Actor actor1(){
-        Actor actor = new Actor("Yash","male",35);
+    public Actor actor1() {
+        Actor actor = new Actor("Yash", "male", 35);
         return actor;
     }
 
     @Bean("movie1")
-    public Movie movie1(){
+    @Scope(value="prototype")
+    public Movie movie1() {
         return new Movie(actor1());
     }
 
-//    @Bean("actor2")
-//    public Actor actor2(){
-//        Actor actor = new Actor("DiCaprio","male",43);
-//        return actor;
-//    }
-//    @Bean("actor3")
-//    public Actor actor3(){
-//        Actor actor = new Actor("Paul Rudd","male",48);
-//        return actor;
-//    }
+    @Bean("actor2")
+    public Actor actor2() {
+        Actor actor = new Actor("DiCaprio", "male", 43);
+        return actor;
+    }
 
-//
-//    @Bean("movie2")
-//    public Movie movie2(){
-//        return new Movie(actor2());
-//    }
-//
-//    @Bean("movie3")
-//    public Movie movie3(){
-//        return new Movie(actor3());
+    @Bean("actor3")
+    @Scope(value="prototype")
+    public Actor actor3() {
+        Actor actor = new Actor("Paul Rudd", "male", 48);
+        return actor;
+    }
+
+
+    @Bean(name = "movie2")
+    public Movie movie2() {
+        return new Movie(actor2());
+    }
+
+    @Bean("movie3")
+    public Movie movie3() {
+        return new Movie(actor3());
+    }
 
 }
+
 
--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -10,8 +10,16 @@ public class main {
     public static void main(String[] args){
 
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
-        Movie movie1=context.getBean(Movie.class);
-          System.out.println(movie1);
+        Movie movie1=(Movie) context.getBean("movie1");
+//          System.out.println(movie1);
 
+        Movie movie4=(Movie)context.getBean("movie1");
+//        System.out.println(movie4);
+
+        System.out.println(context.getBean("movie1"));
+        System.out.println(context.getBean("movie2"));
+        System.out.println(context.getBean("movie3"));
+
+        System.out.println(movie1==movie4);
     }
 }
